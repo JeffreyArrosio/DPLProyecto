29f7891be8f08e372f0ff88fb96178e7eb1138d4
JeffreyArrosio <jeffrey.aj98@gmail.com>
2024-11-05 00:37:26 +0000

    Probando Reload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Despliegue Prueba</title>
+        <title>PROBANDO RELOAD</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
